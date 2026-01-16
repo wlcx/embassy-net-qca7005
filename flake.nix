79be781983475d0dd19ff423bb97d0bed9071220
Sam Willcocks <sam@wlcx.cc>
2026-01-16 01:28:23 +0000

    boilerplate

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Another cool rust disaster from samw.";
+
+  inputs = {
+    crane.url = "github:ipetkov/crane";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    crane,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      craneLib = crane.mkLib pkgs;
+      src = craneLib.cleanCargoSource ./.;
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
+        # Any extra buildInputs
+        buildInputs = [];
+
+      };
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+      thisCrate = craneLib.buildPackage (
+        commonArgs // {
+          inherit cargoArtifacts;
+        }
+      );
+    in {
+      packages.default = thisCrate;
+      apps.default = utils.lib.mkApp {drv = thisCrate;};
+
+      # Provide a dev env with rust and rust-analyzer
+      devShells.default = craneLib.devShell {
+        # extra dev packages
+        packages = [];
+      };
+      formatter = pkgs.alejandra;
+    });
+}
